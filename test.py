282e38478044eb981ccd1f2c7ba1c6bc12ca6b17
SupawitSaelim <s6506022420053@email.kmutnb.ac.th>
2022-12-06 12:04:22 +0700

    up

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
-def main():
-    print('Hello Database')
+import mysql.connector
 
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'Oat0811609596mysql.',
+    database = 'mydatabase'
+)
 
-main()
+mycursor = mydb.cursor()
+
+sql = "UPDATE customers SET address = 'canyon 123' WHERE address = 'valley 345'"
+mycursor.execute(sql)
+mydb.commit()
+
+print(mycursor.rowcount,'record(s) affected')
