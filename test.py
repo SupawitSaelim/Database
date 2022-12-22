4bb40c56dd85ba68375af9e65d6fe02d71d8ecc0
SupawitSaelim <s6506022420053@email.kmutnb.ac.th>
2022-12-22 11:04:56 +0700

    up

--- test.py
+++ test.py
@@ -1,16 +1,5 @@
-import mysql.connector
+class MyClass:
+    x = 5
 
-mydb = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    password = 'Oat0811609596mysql.',
-    database = 'mydatabase'
-)
-
-mycursor = mydb.cursor()
-
-sql = "UPDATE customers SET address = 'canyon 123' WHERE address = 'valley 345'"
-mycursor.execute(sql)
-mydb.commit()
-
-print(mycursor.rowcount,'record(s) affected')
+p1 = MyClass()
+print(p1.x)
